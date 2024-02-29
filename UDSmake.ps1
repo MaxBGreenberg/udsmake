2e4f8c45e807ed4264b772a67582b4f1e57dea10
MaxBGreenberg <maxbgreenberg@gmail.com>
2024-02-29 12:36:11 -0500

    removed opening recompiled PDF in Abode Acrobat Replaced with print command Prints 6 copies of the recompiled PDF to the default system printer

--- UDSmake.ps1
+++ UDSmake.ps1
@@ -1,5 +1,5 @@
 ps Acrobat -ErrorAction SilentlyContinue | kill -PassThru
 cd $env:USERPROFILE
 pdflatex UDS_tickets.tex
-$env:USERPROFILE\UDS_tickets.pdf
+print /#6 UDS_tickets.pdf
 taskkill /im powershell.exe
