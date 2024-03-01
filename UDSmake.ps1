c1e35b7611a17be91990075232263d9ab44204f5
MaxBGreenberg <maxbgreenberg@gmail.com>
2024-03-01 11:25:36 -0500

    Open file UDS_tickets.pdf comment out printing of said file

--- UDSmake.ps1
+++ UDSmake.ps1
@@ -1,5 +1,6 @@
 ps Acrobat -ErrorAction SilentlyContinue | kill -PassThru
 cd $env:USERPROFILE
 pdflatex UDS_tickets.tex
-print /#6 UDS_tickets.pdf
+.\UDS_tickets.pdf
+#print /#6 UDS_tickets.pdf
 taskkill /im powershell.exe
