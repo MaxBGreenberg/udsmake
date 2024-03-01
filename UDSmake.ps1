4edd05c8e958e1324e6f33ab1e19cd3fa5ce9ba3
MaxBGreenberg <maxbgreenberg@gmail.com>
2024-03-01 11:35:12 -0500

    Add explanatory notes

--- UDSmake.ps1
+++ UDSmake.ps1
@@ -1,6 +1,8 @@
-ps Acrobat -ErrorAction SilentlyContinue | kill -PassThru
-cd $env:USERPROFILE
-pdflatex UDS_tickets.tex
-.\UDS_tickets.pdf
-#print /#6 UDS_tickets.pdf
-taskkill /im powershell.exe
+# PowerShell script to update and open file UDS_tickets.pdf
+# Uses LaTeX file which will print current month and year when compiled
+ps Acrobat -ErrorAction SilentlyContinue | kill -PassThru		# Check if Abode accrobat is running and, if it is, kill it
+cd $env:USERPROFILE							# Change to user's home directory
+pdflatex UDS_tickets.tex						# Compile UDS_ticket.tex to PDF
+.\UDS_tickets.pdf							# Open UDS_tickets.pdf in default PDF viewer
+#print /#6 UDS_tickets.pdf						# Print 6 copes of UDS_tickets.pdf to default printer
+taskkill /im powershell.exe						# Kill PowerShell
